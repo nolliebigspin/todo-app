17bd1b4afd319589d5ac76ab26e8ece5c09b54a1
nolliebigspin <alec007@t-online.de>
2019-05-17 13:42:18 +0200

    Added Layout and first Methods
    to fix next: addTodo() to submit a todo

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete">
+      {{ todo.title }}
+      <a class="delete" @click="$emit('del-todo', todo.id)">X</a>
+    </p>
+  </div>
+</template>
+
+<script>
+import { METHODS } from "http";
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px #ccc dotted;
+}
+
+.is-complete {
+  text-decoration: line-through;
+}
+
+.delete {
+  background: #ff0000;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  float: right;
+}
+</style>
