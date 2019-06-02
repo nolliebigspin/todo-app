483f76b69cadcf74445b1a46f5c01b132a075213
PhilsLab <15452526+PhilsLab@users.noreply.github.com>
2019-06-02 14:04:42 +0200

    add production tip config

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,12 @@ module.exports = {
     chainWebpack: config => {
         // no more vendor chunk file, pack everything into one file
         config.optimization.delete('splitChunks')
+
+        // disable production tip in production
+        if (process.env.NODE_ENV === 'production') {
+            config.productionTip = false;
+        } else {
+            config.productionTip = true;
+        }
     }
 }
